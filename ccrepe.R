b9162871be2aa447d322c0256fa49542036a82a7
wanglu2014 <wanglu2014@sibcb.ac.cn>
2018-03-15 12:52:52 +0800

    Update ccrepe.R
    删掉最后两行，看看有什么变化

--- ccrepe.R
+++ ccrepe.R
@@ -11,5 +11,4 @@ ccrepe_matr<-ccrepe(as.matrix(abudata), iterations=100, min.subj=7)
 ccrepe_matr$p.values[!upper.tri(ccrepe_matr$p.values, diag=F)]<-NA
 ccrepe_matr$sim.score[!upper.tri(ccrepe_matr$sim.score, diag=F)]<-NA
 merged_value <- data.frame(i=rep(row.names(ccrepe_matr$sim.score),ncol(ccrepe_matr$sim.score)),j=rep(colnames(ccrepe_matr$sim.score),each=nrow(ccrepe_matr$sim.score)), corvalue=as.vector(ccrepe_matr$sim.score),Pvalue=as.vector(ccrepe_matr$p.values))
-filter_value<-merged_value%>%filter(Pvalue<0.05)
-write.csv(filter_value,'groupOccrepe.csv')
+
